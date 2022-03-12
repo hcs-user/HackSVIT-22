0bdee043b3e54b0eb6af324c14496c20a8d4a2ad
Harsh2220 <harshgajjar145@gmail.com>
2022-03-12 10:46:07 +0530

    map

--- src/components/ThemeCard.tsx
+++ src/components/ThemeCard.tsx
@@ -7,6 +7,15 @@ import {
 	OpenDoodle,
 	SocialWelfareDoodle,
 } from "./svgs";
+
+const doodle = [
+	<BlockchainDoodle h='60' w='60' />,
+	<EducationDoodle h='60' w='60' />,
+	<FinanceDoodle h='60' w='60' />,
+	<OpenDoodle h='60' w='60' />,
+	<SocialWelfareDoodle h='60' w='60' />
+]
+
 export default function ThemeCard() {
 	return (
 		<>
@@ -15,15 +24,15 @@ export default function ThemeCard() {
 				slidesPerView={3}
 				grabCursor={true}
 				breakpoints={{
-          280:{
-            slidesPerView: 1.2,
-          },
-          620:{
-            slidesPerView: 1.8,
-          },
-          720:{
-            slidesPerView: 2.2,
-          },
+					280: {
+						slidesPerView: 1.2,
+					},
+					620: {
+						slidesPerView: 1.8,
+					},
+					720: {
+						slidesPerView: 2.2,
+					},
 					960: {
 						slidesPerView: 2.8,
 					},
@@ -32,69 +41,22 @@ export default function ThemeCard() {
 					},
 				}}
 			>
-				<SwiperSlide>
-					<Flex
-						flexDirection='column'
-						bg='gray.700'
-						rounded='xl'
-						w='fit-content'
-						px='8'
-						py='6'
-					>
-						<EducationDoodle w='60' h='60' />
-					</Flex>
-				</SwiperSlide>
-				<SwiperSlide>
-					<Flex
-						flexDirection='column'
-						bg='gray.700'
-						rounded='xl'
-						w='fit-content'
-						px='8'
-						py='6'
-					>
-						<BlockchainDoodle w='60' h='60' />
-					</Flex>
-				</SwiperSlide>
-
-				<SwiperSlide>
-					<Flex
-						flexDirection='column'
-						bg='gray.700'
-						rounded='xl'
-						w='fit-content'
-						px='8'
-						py='6'
-					>
-						<SocialWelfareDoodle w='60' h='60' />
-					</Flex>
-				</SwiperSlide>
-
-				<SwiperSlide>
-					<Flex
-						flexDirection='column'
-						bg='gray.700'
-						rounded='xl'
-						w='fit-content'
-						px='8'
-						py='6'
-					>
-						<FinanceDoodle w='60' h='60' />
-					</Flex>
-				</SwiperSlide>
-
-				<SwiperSlide>
-					<Flex
-						flexDirection='column'
-						bg='gray.700'
-						rounded='xl'
-						w='fit-content'
-						px='8'
-						py='6'
-					>
-						<OpenDoodle w='60' h='60' />
-					</Flex>
-				</SwiperSlide>
+				{doodle && doodle.map((el) => {
+					return (
+						<SwiperSlide>
+							<Flex
+								flexDirection='column'
+								bg='gray.700'
+								rounded='xl'
+								w='fit-content'
+								px='8'
+								py='6'
+							>
+								{el}
+							</Flex>
+						</SwiperSlide>
+					)
+				})}
 			</Swiper>
 		</>
 	);
