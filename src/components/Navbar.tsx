f0b5706cebfe1a229bf335c5d7d9bf5a3b223ac3
Darshil Rathod <69842641+RathodDarshil@users.noreply.github.com>
2022-03-23 18:53:42 +0530

    menu commented

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,38 @@
-import { Box, Button, Container, Flex, Menu, MenuButton, MenuItem, MenuList, Portal, useDisclosure } from "@chakra-ui/react";
+import {
+	Box,
+	Button,
+	Container,
+	Flex,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuList,
+	Portal,
+	useDisclosure,
+} from "@chakra-ui/react";
 import { MonogramLogo } from "./Branding";
 import NavMenu from "./NavMenu";
 
 export default function Navbar() {
 	const { isOpen, onOpen, onClose } = useDisclosure();
-	const List = ['Home', 'Theme', 'Speakers', 'prizes', 'About'];
+	const List = ["Home", "Theme", "Speakers", "prizes", "About"];
 
 	return (
-		<Box pos='fixed' top='0' left='0' w='full' zIndex='10' bg={isOpen ? 'blue.300' : 'transparent'} backdropFilter="blur(5px)">
+		<Box
+			pos='fixed'
+			top='0'
+			left='0'
+			w='full'
+			zIndex='10'
+			bg={isOpen ? "blue.300" : "transparent"}
+			backdropFilter='blur(5px)'
+		>
 			<Container as={Flex} maxW='container.xl' justify='space-between' py='5'>
 				<MonogramLogo h='10' w='auto' />
 				{/* <Button size='lg' variant='ghost' onClick={isOpen ? onClose : onOpen}>
 					Menu
 				</Button> */}
-				<Menu size='sm'>
+				{/* <Menu size='sm'>
 					<MenuButton>Menu</MenuButton>
 					<Portal>
 						<MenuList zIndex='20'>
@@ -24,9 +43,9 @@ export default function Navbar() {
 							})}
 						</MenuList>
 					</Portal>
-				</Menu>
+				</Menu> */}
 			</Container>
-			<NavMenu isOpen={isOpen} onClose={onClose} />
+			{/* <NavMenu isOpen={isOpen} onClose={onClose} /> */}
 		</Box>
 	);
 }
