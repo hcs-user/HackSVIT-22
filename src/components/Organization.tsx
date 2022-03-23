9dd91df1da5adc543fb852d518f5c88f4be4c495
Harsh2220 <harshgajjar145@gmail.com>
2022-03-23 10:04:06 +0530

    oraganization logo updated

--- src/components/Organization.tsx
+++ src/components/Organization.tsx
@@ -6,7 +6,7 @@ export default function Organization() {
         <Flex gap='20' py='10' flexWrap='wrap' justifyContent={["center", null, "start"]}>
             <Img src='hcs.png' h='200' w='auto' />
             <Img src='codingclub.png' h='200' w='auto' />
-            <Img src='hcs.png' h='200' w='auto' />
+            <Img src='svit.png' h='200' w='auto' />
         </Flex>
     )
 }
