a76840bc4f78ea1fdf1d9067b87328458a9a6312
RathodDarshil <darshilrathod0@gmail.com>
2022-03-23 15:34:56 +0530

    darshil changes

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,36 +1,46 @@
-import { Box, Flex, HStack, Image, Link } from '@chakra-ui/react'
-import { motion } from 'framer-motion';
-import Marquee from 'react-fast-marquee';
-import SPONSORS from "../data/sponsors.json";
+import {
+	Box,
+	Flex,
+	Heading,
+	HStack,
+	Image,
+	Link,
+	VStack,
+} from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import Marquee from "react-fast-marquee";
+import SPONSORS from "../data/sponsor.json";
 
 const MotionBox = motion(Box);
 
 export default function Sponsors() {
-    return (
-        <Marquee gradient={false} speed={100}>
-            <HStack>
-                {SPONSORS.map((el) => (                    
-                    <MotionBox>
-                        <Link
-                            target="_blank"
-                            href={el.siteUrl}
-                            as={Flex}
-                            m="3"
-                            p="2"
-                            bg={el.bg}
-                            rounded="lg"
-                            userSelect="none"
-                            _hover={{ transform: "scale(1.1)" }}
-                        >
-                            <Image
-                                h="16"
-                                rounded="lg"
-                                src={el.iconPath}
-                            />
-                        </Link>
-                    </MotionBox>
-                ))}
-            </HStack>
-        </Marquee>
-    )
+	return (
+		<VStack alignSelf='flex-start'>
+			{SPONSORS.map((type) => (
+				<VStack alignSelf='flex-start' flexWrap='wrap'>
+					<Heading>{type.category}</Heading>
+
+					<HStack flexWrap='wrap'>
+						{type.company.map((el) => (
+							<MotionBox>
+								<Link
+									target='_blank'
+									href={el.siteUrl}
+									as={Flex}
+									m='3'
+									p='2'
+									bg={el.bg}
+									rounded='lg'
+									userSelect='none'
+									_hover={{ transform: "scale(1.1)" }}
+								>
+									<Image h='16' rounded='lg' src={el.iconPath} />
+								</Link>
+							</MotionBox>
+						))}
+					</HStack>
+				</VStack>
+			))}
+		</VStack>
+	);
 }
