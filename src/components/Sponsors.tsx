df2a28e8af0de9c9b096920f9bbff1c3919a1918
Vraj <thevrajshah@gmail.com>
2022-03-31 20:09:15 +0530

    layout fixes

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -7,6 +7,7 @@ import {
 	LinkBox,
 	LinkOverlay,
 	Text,
+	Tooltip,
 	VStack,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
@@ -19,27 +20,29 @@ export default function Sponsors() {
 		<VStack alignItems='flex-start'>
 			{SPONSORS.map((type) => (
 				<VStack alignItems='flex-start' py='1'>
-					<Text my='2' fontSize='2xl'>{type.category}</Text>
+					<Text my='2' fontSize='2xl'>
+						{type.category}
+					</Text>
 					<Flex rowGap='6' columnGap='3' flexWrap='wrap' alignItems='flex-start'>
 						{type.company.map((el) => (
-							<MotionBox alignItems='flex-start'>
-								<LinkBox
-									as={Flex}
-									m='3'
-									p='2'
-									rounded='lg'
-									userSelect='none'
-								>
-									<LinkOverlay target='_blank' href={el.siteUrl}>
-										<Img h={el.name == "SVIT" ? '130px' : '16'} rounded='lg' src={el.iconPath} />
-									</LinkOverlay>
-								</LinkBox>
-							</MotionBox>
+							<Tooltip hasArrow label={el.name} placement='bottom'>
+								<MotionBox alignItems='flex-start' in>
+									<LinkBox as={Flex} m='3' p='2' rounded='lg' userSelect='none'>
+										<LinkOverlay target='_blank' href={el.siteUrl}>
+											<Img
+												h={el.name == "SVIT" ? "48" : "12"}
+												w='auto'
+												rounded='lg'
+												src={el.iconPath}
+											/>
+										</LinkOverlay>
+									</LinkBox>
+								</MotionBox>
+							</Tooltip>
 						))}
 					</Flex>
 				</VStack>
-			))
-			}
-		</VStack >
+			))}
+		</VStack>
 	);
 }
--- src/components/ThemeCard.tsx
+++ src/components/ThemeCard.tsx
@@ -10,34 +10,62 @@ import {
 } from "./svgs";
 
 const doodle = [
-	{ doodle: <BlockchainDoodle h={[40, null, 48]} w={[40, null, 48]} />, name: "BlockChain" },
-	{ doodle: <EducationDoodle h={[40, null, 48]} w={[40, null, 48]} />, name: "Education" },
-	{ doodle: <FinanceDoodle h={[40, null, 48]} w={[40, null, 48]} />, name: "FinTech" },
-	{ doodle: <ClimateDoodle h={[40, null, 48]} w={[40, null, 48]} />, name: "Climate Change" },
-	{ doodle: <SocialWelfareDoodle h={[40, null, 48]} w={[40, null, 48]} />, name: "Social Welfare" },
-	{ doodle: <OpenDoodle h={[40, null, 48]} w={[40, null, 48]} />, name: "Open Innovation" },
-]
+	{
+		doodle: <BlockchainDoodle h={[40, null, 48]} w={[40, null, 48]} />,
+		name: "BlockChain",
+	},
+	{
+		doodle: <EducationDoodle h={[40, null, 48]} w={[40, null, 48]} />,
+		name: "Education",
+	},
+	{
+		doodle: <FinanceDoodle h={[40, null, 48]} w={[40, null, 48]} />,
+		name: "FinTech",
+	},
+	{
+		doodle: <ClimateDoodle h={[40, null, 48]} w={[40, null, 48]} />,
+		name: "Climate Change",
+	},
+	{
+		doodle: <SocialWelfareDoodle h={[40, null, 48]} w={[40, null, 48]} />,
+		name: "Social Welfare",
+	},
+	{
+		doodle: <OpenDoodle h={[40, null, 48]} w={[40, null, 48]} />,
+		name: "Open Innovation",
+	},
+];
 
 export default function ThemeCard() {
 	return (
 		<>
-			<Flex gap='12' py='10' flexWrap='wrap' justifyContent={["center", null, "center"]}>
-				{doodle && doodle.map((el) => {
-					return (
-						<Flex
-							flexDirection='column'
-							alignItems='center'
-							bg='gray.700'
-							rounded='xl'
-							w='fit-content'
-							px='8'
-							py='6'
-						>
-							{el.doodle}
-							<Text mt='6' fontSize={['lg', null, '2xl']}>{el.name}</Text>
-						</Flex>
-					)
-				})}
+			<Flex
+				gap='12'
+				py='10'
+				flexWrap='wrap'
+				justifyContent={["center", null, "center"]}
+			>
+				{doodle &&
+					doodle.map((el) => {
+						return (
+							<Flex
+								flexDirection='column'
+								alignItems='center'
+								bg='gray.700'
+								rounded='xl'
+								// w='fit-content'
+								px='8'
+								py='6'
+								h='300'
+								minW='300'
+							>
+								{el.doodle}
+								<Text mt='6' fontSize={["lg", null, "2xl"]}>
+									{el.name}
+								</Text>
+							</Flex>
+						);
+					})}
 			</Flex>
 		</>
 	);
