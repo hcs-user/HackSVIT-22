891ee1fdb8e7a3d33dabd89fd87702eb7fc4d169
smitsgithub <githubforsmit@gmail.com>
2022-04-01 23:36:49 +0530

    Footer Updated

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,15 +1,6 @@
 import { Avatar, Box, Heading, HStack, Text, Tooltip } from '@chakra-ui/react'
 import TEAM from '../data/team.json';
 
-const Label = (name: String, post: String) => {
-    return (
-        <Box textAlign='center'>
-            <Text fontSize='md'>{name}</Text>
-            <Text textTransform='uppercase' fontWeight='bold'>{post}</Text>
-        </Box>
-    )
-}
-
 export default function Team() {
     return (
         <>
@@ -18,100 +9,14 @@ export default function Team() {
                     <>
                         {
                             team.member.map((member) => (
-                                <Tooltip key={member.name} label={Label(member.name, team.team_name)} hasArrow arrowSize={12} bg='white'>
-                                    <Avatar size='2xl' name={member.name} src='https://bit.ly/dan-abramov' />
+                                <Tooltip key={member.name} label={member.name} hasArrow arrowSize={12} bg='white'>
+                                    <Avatar size='2xl' name={member.name} src={member.img}/>
                                 </Tooltip>
                             ))
                         }
                     </>
                 ))}
             </HStack>
-            {/* <HStack gap='12' flexWrap='wrap' justifyContent='center'>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-            </HStack>
-            <HStack gap='12' my='4' pl={[null, '2']} flexWrap='wrap' justifyContent='center'>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-            </HStack>
-            <HStack gap='12' flexWrap='wrap' justifyContent='center'>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-            </HStack>
-            <HStack gap='12' my='4' pl={[null, '2']} flexWrap='wrap' justifyContent='center'>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-                <Tooltip label={Label("Harsh Sachaniya", "Developer")} hasArrow arrowSize={12} bg='white'>
-                    <Avatar size='2xl' name='Harsh Sachaniya' src='https://bit.ly/dan-abramov' />
-                </Tooltip>
-            </HStack> */}
         </>
     )
 }
