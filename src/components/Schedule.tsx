eee5426f49aa1ae0073cd44c6b2f4a18b68a4fa0
Harsh2220 <harshgajjar145@gmail.com>
2022-04-21 18:59:21 +0530

    path added

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -31,6 +31,9 @@ export default function Schedule() {
 		<Container maxW='container.xl'>
 			<Flex justify='space-between' align='center'>
 				<Box>
+					{/* <Box as='svg' viewBox='0 0 381.09 195.074' mb='2' me='8'>
+						<path id="Path_16" data-name="Path 16" d="M1465.254,6107.661c92.535-77.88,363.022,66.563,363.022-41.871s-245.783,14.236-220.661-133.987" transform="translate(-1454.686 -5923.155)" fill="none" stroke="#fb0" stroke-linecap="round" stroke-linejoin="round" stroke-width="15" />
+					</Box> */}
 					<Heading size='2xl'>Schedule</Heading>
 					<Text color='gray.400' fontSize='sm' pt='2'>
 						*All the mentioned times are in IST timezone
@@ -51,92 +54,25 @@ export default function Schedule() {
 					/>
 				</HStack>
 			</Flex>
-			{breakpoint == "lg" || breakpoint == "xl" || breakpoint == "2xl" ? (
-				<Flex gap='6' py='8'>
-					{SCHEDULE.map(({ date, activities }, i) => (
-						<>
-							<VStack w='full' align='start'>
-								<Text ml='5'>
-									{/* <Text as='span' fontWeight='400'>
+			{
+				breakpoint == "lg" || breakpoint == "xl" || breakpoint == "2xl" ? (
+					<Flex gap='6' py='8'>
+						{SCHEDULE.map(({ date, activities }, i) => (
+							<>
+								<VStack w='full' align='start'>
+									<Text ml='5'>
+										{/* <Text as='span' fontWeight='400'>
 										Day {i + 1}.
 									</Text>{" "}
 									{format(new Date(date), "MMM d, cccc")} */}
-									<Text fontSize='2xl'>Day {i + 1}.</Text>
-									<Heading fontSize='3xl' color='orange.300'>
-										{format(new Date(date), "MMMM d, cccc")}
-									</Heading>
-								</Text>
-								<VStack pt='4' w='full' align='start'>
-									{activities.map(({ startTime, activity, valid }, j) => {
-										const dateTime = date + " " + startTime;
-										// const active = formatDistance(
-										// 	new Date(date + " " + SCHEDULE[i].activities[j + 1].startTime),
-										// 	new Date(dateTime)
-										// );
-
-										// const active = i == 0 && j == 0;
-										// console.log(active);
-
-										return (
-											valid && (
-												<Flex
-													key={j}
-													px='3'
-													py='2'
-													// bg={active ? "yellow.500" : undefined}
-													rounded='base'
-													w='full'
-													align='center'
-													transition='0.3s'
-													_hover={{ bg: "yellow.500" }}
-												>
-													<Text
-														fontSize='lg'
-														me={4}
-														minW='20'
-														textAlign='right'
-														fontWeight='200'
-													>
-														{format(new Date(dateTime), "hh:mm a")}
-													</Text>
-													<Heading fontSize='lg' fontWeight='600'>
-														{activity}{" "}
-													</Heading>
-												</Flex>
-											)
-										);
-									})}
-								</VStack>
-							</VStack>
-						</>
-					))}
-				</Flex>
-			) : (
-				// {SCHEDULE.map(({ date, activities }, i) => (
-				<>
-					<VStack w='full' align='start' my='2'>
-						<AnimatePresence exitBeforeEnter>
-							<MotionBox
-								initial={{ opacity: 0 }}
-								animate={{ opacity: 1 }}
-								exit={{ opacity: 0 }}
-								key={valueAsNumber}
-							>
-								{/* <Text> */}
-								{/* <Text as='span' fontWeight='400'>
-									Day {i + 1}.
-								</Text>{" "}
-								{format(new Date(date), "MMM d, cccc")} */}
-								<Text fontSize='2xl'>Day {valueAsNumber + 1}.</Text>
-								<Heading fontSize='3xl' color='orange.300'>
-									{format(new Date(SCHEDULE[valueAsNumber].date), "MMMM d, cccc")}
-								</Heading>
-								{/* </Text> */}
-
-								<VStack spacing='3' pt='4' w='full' align='start'>
-									{SCHEDULE[valueAsNumber].activities.map(
-										({ startTime, activity, valid }, j) => {
-											const dateTime = SCHEDULE[valueAsNumber].date + " " + startTime;
+										<Text fontSize='2xl'>Day {i + 1}.</Text>
+										<Heading fontSize='3xl' color='orange.300'>
+											{format(new Date(date), "MMMM d, cccc")}
+										</Heading>
+									</Text>
+									<VStack pt='4' w='full' align='start'>
+										{activities.map(({ startTime, activity, valid }, j) => {
+											const dateTime = date + " " + startTime;
 											// const active = formatDistance(
 											// 	new Date(date + " " + SCHEDULE[i].activities[j + 1].startTime),
 											// 	new Date(dateTime)
@@ -149,12 +85,14 @@ export default function Schedule() {
 												valid && (
 													<Flex
 														key={j}
-														px='2'
-														py='1'
+														px='3'
+														py='2'
 														// bg={active ? "yellow.500" : undefined}
 														rounded='base'
 														w='full'
 														align='center'
+														transition='0.3s'
+														_hover={{ bg: "yellow.500" }}
 													>
 														<Text
 															fontSize='lg'
@@ -171,15 +109,82 @@ export default function Schedule() {
 													</Flex>
 												)
 											);
-										}
-									)}
+										})}
+									</VStack>
 								</VStack>
-							</MotionBox>
-						</AnimatePresence>
-					</VStack>
-				</>
-				// ))}
-			)}
-		</Container>
+							</>
+						))}
+					</Flex>
+				) : (
+					// {SCHEDULE.map(({ date, activities }, i) => (
+					<>
+						<VStack w='full' align='start' my='2'>
+							<AnimatePresence exitBeforeEnter>
+								<MotionBox
+									initial={{ opacity: 0 }}
+									animate={{ opacity: 1 }}
+									exit={{ opacity: 0 }}
+									key={valueAsNumber}
+								>
+									{/* <Text> */}
+									{/* <Text as='span' fontWeight='400'>
+									Day {i + 1}.
+								</Text>{" "}
+								{format(new Date(date), "MMM d, cccc")} */}
+									<Text fontSize='2xl'>Day {valueAsNumber + 1}.</Text>
+									<Heading fontSize='3xl' color='orange.300'>
+										{format(new Date(SCHEDULE[valueAsNumber].date), "MMMM d, cccc")}
+									</Heading>
+									{/* </Text> */}
+
+									<VStack spacing='3' pt='4' w='full' align='start'>
+										{SCHEDULE[valueAsNumber].activities.map(
+											({ startTime, activity, valid }, j) => {
+												const dateTime = SCHEDULE[valueAsNumber].date + " " + startTime;
+												// const active = formatDistance(
+												// 	new Date(date + " " + SCHEDULE[i].activities[j + 1].startTime),
+												// 	new Date(dateTime)
+												// );
+
+												// const active = i == 0 && j == 0;
+												// console.log(active);
+
+												return (
+													valid && (
+														<Flex
+															key={j}
+															px='2'
+															py='1'
+															// bg={active ? "yellow.500" : undefined}
+															rounded='base'
+															w='full'
+															align='center'
+														>
+															<Text
+																fontSize='lg'
+																me={4}
+																minW='20'
+																textAlign='right'
+																fontWeight='200'
+															>
+																{format(new Date(dateTime), "hh:mm a")}
+															</Text>
+															<Heading fontSize='lg' fontWeight='600'>
+																{activity}{" "}
+															</Heading>
+														</Flex>
+													)
+												);
+											}
+										)}
+									</VStack>
+								</MotionBox>
+							</AnimatePresence>
+						</VStack>
+					</>
+					// ))}
+				)
+			}
+		</Container >
 	);
 }
